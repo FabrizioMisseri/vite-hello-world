3503e8dc4002faaa3bb27536da46de3cf3032798
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-07 19:24:57 +0100

    refactoring

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+
+
+<template>
+  <h1>Hello world con vite + vue</h1>
+</template>
+
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
